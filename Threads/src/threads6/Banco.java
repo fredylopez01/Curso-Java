8df860866ddb5e4f4d9d87a6ea66ed1e4b353f4e
Fredy Lopez <133995369+fredylopez01@users.noreply.github.com>
2023-12-09 19:40:18 -0500

    Uso de la palabra reservada synchronized, y los métodos wait y notifyAll, para simplificar el uso de Threads, funciona igualmente

--- Threads/src/threads6/Banco.java
+++ Threads/src/threads6/Banco.java
@@ -1,39 +1,44 @@
 package threads6;
 
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+//import java.util.concurrent.locks.Condition;
+//import java.util.concurrent.locks.Lock;
+//import java.util.concurrent.locks.ReentrantLock;
 
 public class Banco {
 	private final double[] cuentas;
-	private Lock cierreBanco;
-	private Condition saldoSuficiente;
+//	private Lock cierreBanco;
+//	private Condition saldoSuficiente;
 	
 	public Banco() {
-		cierreBanco = new ReentrantLock();
+//		cierreBanco = new ReentrantLock();
 		cuentas = new double[100];
 		for(int i = 0; i < cuentas.length; i++) {
 			cuentas[i] = 2000;
 		}
-		saldoSuficiente = cierreBanco.newCondition();
+//		saldoSuficiente = cierreBanco.newCondition();
 	}
 	
-	public void transferencia(int cuentaOrigen, int cuentaDestino, double cantidad) throws InterruptedException {
-		cierreBanco.lock(); //Método para bloquear este código y que solo pueda ser ejecutado por un hilo
-		try {
+/*	Se deja de utilizar la instancia de Condition y Lock 
+ * ya que se utiliza la palabra reservada synchronized, por lo tanto, 
+ * se deja comentado*/
+	public synchronized void transferencia(int cuentaOrigen, int cuentaDestino, double cantidad) throws InterruptedException {
+//		cierreBanco.lock(); //Método para bloquear este código y que solo pueda ser ejecutado por un hilo
+//		try {
 			while(cuentas[cuentaOrigen]<cantidad) {
 //				return; //si el saldo es menor a la cantidad entonces termina el método
-				saldoSuficiente.await();
+//				saldoSuficiente.await();
+				wait();
 			}
 			System.out.println(Thread.currentThread());
 			cuentas[cuentaOrigen]-= cantidad;
 			System.out.printf("%10.2f de %d para %d", cantidad, cuentaOrigen, cuentaDestino);
 			cuentas[cuentaDestino]+= cantidad;
 			System.out.printf("Saldo total: %10.2f%n", getSaldoTotal());
-			saldoSuficiente.signalAll();
-		} finally {
-			cierreBanco.unlock(); //El método se desbloquea para que 
-		}
+//			saldoSuficiente.signalAll();
+			notifyAll();
+//		} finally {
+//			cierreBanco.unlock(); //El método se desbloquea para que 
+//		}
 	}
 	
 	public double getSaldoTotal() {
