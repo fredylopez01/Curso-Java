e5cc76a11b7058ad2c3ba4d104ca7ac47ad1c9e3
Fredy Lopez <133995369+fredylopez01@users.noreply.github.com>
2024-01-02 10:28:28 -0500

    Creo el socket de parte del cliente, que me servira para abrir el flujo de datos para lugo conectar con el servidor

--- Sockets/src/sockets/cliente/PanelCliente.java
+++ Sockets/src/sockets/cliente/PanelCliente.java
@@ -2,6 +2,12 @@ package sockets.cliente;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -26,6 +32,23 @@ public class PanelCliente extends JPanel {
 		add(message, gbc);
 		
 		btnSubmit = new JButton("Enviar");
+		btnSubmit.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try (Socket mySocket = new Socket("192.168.45.168", 9999)) {
+					DataOutputStream flujoSalida = new DataOutputStream(mySocket.getOutputStream());
+					flujoSalida.writeUTF(message.getText());
+					flujoSalida.close();
+				} catch (UnknownHostException e1) {
+					System.out.println(e1.getMessage());
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					System.out.println(e1.getMessage());
+					e1.printStackTrace();
+				}
+			}
+		});
 		gbc.gridy = 2;
 		add(btnSubmit, gbc);
 	}
